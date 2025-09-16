72e6066ffbe2c016c87085f1b3fcdb7501cf670a
Veronika-Gerasimova <144018256+Veronika-Gerasimova@users.noreply.github.com>
2025-09-16 17:56:37 +0800

    Update vite.config.js

--- plane/vite.config.js
+++ plane/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  optimizeDeps: {
+    include: ['js-cookie']
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
