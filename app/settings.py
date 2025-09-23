302b27cde47bb90058998be3156a47a186ab579d
Veronika-Gerasimova <144018256+Veronika-Gerasimova@users.noreply.github.com>
2025-09-23 14:32:33 +0800

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -150,6 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+
+# Папка, куда collectstatic будет складывать все статические файлы
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
